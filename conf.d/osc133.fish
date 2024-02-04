de3bf0c31cf1c07ab9a82bfcf41784ba6c776727
Yuki Ito <acomagu@gmail.com>
2024-02-04 18:03:14 +0900

    Retain status
    Resolves #2

--- conf.d/osc133.fish
+++ conf.d/osc133.fish
@@ -8,6 +8,7 @@ set -g _fishprompt_aid "fish"$fish_pid
 set -g _fishprompt_started 0
 # empty if running; or a numeric exit code; or CANCEL
 set -g _fishprompt_postexec ""
+set -g _saved_pipestatus 0
 
 set _fishprompt_prompt_count 0
 set _fishprompt_disp_count 0
@@ -21,6 +22,7 @@ end
 
 function _fishprompt_postexec --on-event fish_postexec
     set _fishprompt_postexec $status
+    set _saved_pipestatus $pipestatus
 end
 
 function __fishprompt_cancel --on-event fish_cancel
@@ -50,8 +52,72 @@ function _osc133_setup --on-event fish_prompt
 
     function fish_prompt
         set _fishprompt_disp_count (math $_fishprompt_disp_count + 1)
+        _osc133_set_pipestatus $_saved_pipestatus
         printf "\033]133;P;k=i\007%b\033]133;B\007" (string join "\n" (_fishprompt_saved_prompt))
         set _fishprompt_started 1
         set _fishprompt_postexec ""
+    end
+end
+
+function _osc133_set_status
+    return $argv
+end
+
+function _osc133_set_pipestatus
+    switch (count $argv)
+        case 1
+            _osc133_set_status $argv[1]
+        case 2
+            _osc133_set_status $argv[1] \
+            | _osc133_set_status $argv[2]
+        case 3
+            _osc133_set_status $argv[1] \
+            | _osc133_set_status $argv[2] \
+            | _osc133_set_status $argv[3]
+        case 4
+            _osc133_set_status $argv[1] \
+            | _osc133_set_status $argv[2] \
+            | _osc133_set_status $argv[3] \
+            | _osc133_set_status $argv[4]
+        case 5
+            _osc133_set_status $argv[1] \
+            | _osc133_set_status $argv[2] \
+            | _osc133_set_status $argv[3] \
+            | _osc133_set_status $argv[4] \
+            | _osc133_set_status $argv[5]
+        case 6
+            _osc133_set_status $argv[1] \
+            | _osc133_set_status $argv[2] \
+            | _osc133_set_status $argv[3] \
+            | _osc133_set_status $argv[4] \
+            | _osc133_set_status $argv[5] \
+            | _osc133_set_status $argv[6]
+        case 7
+            _osc133_set_status $argv[1] \
+            | _osc133_set_status $argv[2] \
+            | _osc133_set_status $argv[3] \
+            | _osc133_set_status $argv[4] \
+            | _osc133_set_status $argv[5] \
+            | _osc133_set_status $argv[6] \
+            | _osc133_set_status $argv[7]
+        case 8
+            _osc133_set_status $argv[1] \
+            | _osc133_set_status $argv[2] \
+            | _osc133_set_status $argv[3] \
+            | _osc133_set_status $argv[4] \
+            | _osc133_set_status $argv[5] \
+            | _osc133_set_status $argv[6] \
+            | _osc133_set_status $argv[7] \
+            | _osc133_set_status $argv[8]
+        default
+            _osc133_set_status $argv[1] \
+            | _osc133_set_status $argv[2] \
+            | _osc133_set_status $argv[3] \
+            | _osc133_set_status $argv[4] \
+            | _osc133_set_status $argv[5] \
+            | _osc133_set_status $argv[6] \
+            | _osc133_set_status $argv[7] \
+            | _osc133_set_status $argv[8] \
+            | _osc133_set_status $argv[-1]
     end
 end
